b41ef1af42688ddf87ca4c5aba74cbacd3eaf133
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-03 16:47:30 -0700

    Finished function restruant_rating which prints out alphabitized resturant and its rating.

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,1 +1,14 @@
 # your code goes here
+def restaurant_rating(filename):
+    """ Function takes arguement of a file name and iterates line by line over file to print out alphabitized restruant and rating.
+    No return value only outputs print lines."""
+    restaurant_rating_dict = {}
+    for restaurant_data in open(filename):
+        restaurant_data_list = restaurant_data.rstrip().split(":")
+        
+        restaurant_rating_dict[restaurant_data_list[0]] = restaurant_data_list[1]
+    ordered_restruant = sorted(restaurant_rating_dict.items())
+
+    for tup in ordered_restruant:
+        print "{} is rated at {}.".format(tup[0], tup[1])
+restaurant_rating("scores.txt")
